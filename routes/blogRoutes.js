95816de55f656d5629b8112ce6fd9d189b0f281e
PSICOAPOIO <102975161+PSICOAPOIO@users.noreply.github.com>
2022-08-13 01:07:47 -0300

    Add files via upload

---
+++ routes/blogRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const BlogController = require('../controllers/BlogController')
+
+
+
+
+router.get('/meditacao', BlogController.meditacao)
+router.get('/', BlogController.showMeditacao)
+
+router.get('/estresse', BlogController.estresse)
+router.get('/', BlogController.showEstresse)
+
+router.get('/ansiedade', BlogController.ansiedade)
+router.get('/', BlogController.showAnsiedade)
+
+router.get('/depressao', BlogController.depressao)
+router.get('/', BlogController.showDepressao)
+
+router.get('/profissionais', BlogController.profissionais)
+router.get('/', BlogController.showProfissionais)
+
+module.exports = router
